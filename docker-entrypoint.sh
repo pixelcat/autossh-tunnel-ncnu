c2beb3000de1251bb0167ba0e97651d1814b611c
Aaron Stewart <aaron@workhabit.com>
2017-02-28 18:28:32 -0800

    run autossh in foreground

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 echo "Starting autossh tunnel."
-AUTOSSH_CMD="/usr/bin/autossh -M 0 -f -oStrictHostKeyChecking=no ${AUTOSSH_OPTS} -NL \
+AUTOSSH_CMD="/usr/bin/autossh -M 0 -oStrictHostKeyChecking=no ${AUTOSSH_OPTS} -NL \
   ${AUTOSSH_LOCAL_PORT}:${AUTOSSH_TUNNEL_HOST}:${AUTOSSH_REMOTE_PORT} \
   ${AUTOSSH_USER}@${AUTOSSH_TARGET_HOST}"
 echo ${AUTOSSH_CMD}
