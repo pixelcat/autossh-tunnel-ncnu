2306a125ff3ca8372bfa654e3540d9194f1748c6
Aaron Stewart <aaron@workhabit.com>
2017-03-02 17:14:03 -0800

    updated autossh to expose port to docker containers.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 echo "Starting autossh tunnel."
-AUTOSSH_CMD="/usr/bin/autossh -M 0 -oStrictHostKeyChecking=no ${AUTOSSH_OPTS} -NL \
-  ${AUTOSSH_LOCAL_PORT}:${AUTOSSH_TUNNEL_HOST}:${AUTOSSH_REMOTE_PORT} \
+AUTOSSH_CMD="/usr/bin/autossh -M 0 -oGatewayPorts=yes -oStrictHostKeyChecking=no ${AUTOSSH_OPTS} -NL \
+  0.0.0.0:${AUTOSSH_LOCAL_PORT}:${AUTOSSH_TUNNEL_HOST}:${AUTOSSH_REMOTE_PORT} \
   ${AUTOSSH_USER}@${AUTOSSH_TARGET_HOST}"
 echo ${AUTOSSH_CMD}
 
